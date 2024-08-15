e5246469ba4153b51d41ee711b821ce4856c9bc1
Ozinger <chocosdj@naver.com>
2024-08-15 20:32:05 +0900

    Feat : Stat Management

--- Project-KS/Assets/Dongjun/Scripts/UIManager.cs
+++ Project-KS/Assets/Dongjun/Scripts/UIManager.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
+using System.Globalization;
 
 public class UIManager : MonoBehaviour
 {
     public Image[] behaviours;
+    public TMP_Text deadBodiesCount;
+    public TMP_Text kindredPointCount;
 
+    private int kindredPoint;
+    private int deadBodies;
+    private int maxBodies;
     private int currentBehaviour;
 
     // Start is called before the first frame update
     void Start()
     {
+        maxBodies = 20;
+        deadBodies = 0;
         currentBehaviour = 0;  
     }
 
@@ -21,6 +30,11 @@ public class UIManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) ChangeBehaviour(1);
         if (Input.GetMouseButtonDown(1)) ChangeBehaviour(-1);
+
+        if (Input.GetKeyDown(KeyCode.A)) ManageBodies(1);
+        if (Input.GetKeyDown(KeyCode.S)) ManageBodies(-1);
+        if (Input.GetKeyDown(KeyCode.D)) ManageKindredPoint(1);
+        if (Input.GetKeyDown(KeyCode.F)) ManageKindredPoint(-1);
     }
 
     void ChangeBehaviour(int direction)
@@ -30,5 +44,32 @@ public class UIManager : MonoBehaviour
         if (currentBehaviour == -1) currentBehaviour = 2;
         else if (currentBehaviour == 3) currentBehaviour = 0;
         behaviours[currentBehaviour].DOFade(1f, 0.1f).SetEase(Ease.OutSine).SetDelay(0.1f);
+    }
+
+    void ManageBodies(int amount)
+    {
+        if (deadBodies + amount > maxBodies || deadBodies + amount < 0)
+        {
+            Debug.Log("Invalid Access! UIManager.cs -> ManageBodies()");
+        } else 
+        {
+            deadBodies += amount;
+        }
+        deadBodiesCount.text = "Dead Bodies : " + deadBodies.ToString();
+        if (deadBodies == 20) deadBodiesCount.color = Color.red;
+        else deadBodiesCount.color = Color.white;
+    }
+
+    void ManageKindredPoint(int amount)
+    {
+        if (kindredPoint + amount < 0)
+        {
+            Debug.Log("Invalid Access! UIManager.cs -> ManageKindredPoint()");
+        }
+        else
+        {
+            kindredPoint += amount;
+        }
+        kindredPointCount.text = "KindredPoint : " + kindredPoint.ToString();
     }
 }
