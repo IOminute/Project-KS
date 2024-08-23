b2a48fb77b55be7bf01b4559825dcb3e57350363
Ozinger <chocosdj@naver.com>
2024-08-23 13:39:47 +0900

    Fix : NecroCamController

--- Project-KS/Assets/Dongjun/Scripts/Necro/NecroCamController.cs
+++ Project-KS/Assets/Dongjun/Scripts/Necro/NecroCamController.cs
@@ -9,20 +9,20 @@ public class NecroCamController : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
 
         // 화면의 모서리 위치를 체크
-        if (mousePosition.x <= 0) // 왼쪽 모서리
+        if (mousePosition.x <= 50) // 왼쪽 모서리
         {
             transform.position += Vector3.left * moveSpeed * Time.deltaTime;
         }
-        else if (mousePosition.x >= Screen.width) // 오른쪽 모서리
+        else if (mousePosition.x >= Screen.width - 50) // 오른쪽 모서리
         {
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
         }
 
-        if (mousePosition.y <= 0) // 아래쪽 모서리
+        if (mousePosition.y <= 50) // 아래쪽 모서리
         {
             transform.position += new Vector3(0, 0, -1) * moveSpeed * Time.deltaTime;
         }
-        else if (mousePosition.y >= Screen.height) // 위쪽 모서리
+        else if (mousePosition.y >= Screen.height - 50) // 위쪽 모서리
         {
             transform.position += new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime;
         }
