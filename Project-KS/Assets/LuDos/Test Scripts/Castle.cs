a6b6cb5566662f448b4872d0ad3d6d920dfadbac
Heokunho <93777460+Heokunho@users.noreply.github.com>
2024-08-23 23:47:15 +0900

    Revert "Feat: GameOver scene"
    This reverts commit 133795628545b5c2848a152738d366ba2e17a907.

--- Project-KS/Assets/LuDos/Test Scripts/Castle.cs
+++ Project-KS/Assets/LuDos/Test Scripts/Castle.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement; // 씬 관리에 필요한 네임스페이스 추가
 
 public class Castle : MonoBehaviour
 {
     bool isGameOver = false;
-    float health = 10;
+    float health = 1000;
 
     public Image healthBar;
-
     private void TakeDamage(float damageAmount)
     {
         if (!isGameOver)
@@ -23,9 +21,7 @@ public class Castle : MonoBehaviour
             {
                 health = 0;
                 isGameOver = true;
-
-                // 게임 오버 씬으로 전환
-                SceneManager.LoadScene("GameOver");
+                // 성 무너지는 애니메이션?
             }
         }
     }
